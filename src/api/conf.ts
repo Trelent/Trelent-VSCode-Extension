d7b5233ff71f3ecd6f3836cf53d72b972cbe2248
Calum Bird <calum@trelent.net>
2022-05-27 16:45:08 -0300

    Update conf formatting

--- src/api/conf.ts
+++ src/api/conf.ts
@@ -1,11 +1,12 @@
-// Prod
-export const GET_USER_URL               = 'https://prod-api.trelent.net/me';
-export const LOGIN_URL                  = 'https://prod-api.trelent.net/auth/login';
-export const LOGOUT_URL                 = 'https://prod-api.trelent.net/auth/logout';
 export const PARSE_CURRENT_FUNCTION_URL = 'https://lambda.trelent.net/api/v4/ParseCurrent/ParseCurrentFunction';
 export const PARSE_FUNCTIONS_URL        = 'https://lambda.trelent.net/api/v4/ParseAll/ParseSourceCode';
 export const SUBMIT_CHOICE_URL          = 'https://trelent.npkn.net/submit-source';
 export const SUPPORTED_LANGUAGES        = ["csharp", "java", "javascript", "python"];
+
+// Prod
+export const GET_USER_URL               = 'https://prod-api.trelent.net/me';
+export const LOGIN_URL                  = 'https://prod-api.trelent.net/auth/login';
+export const LOGOUT_URL                 = 'https://prod-api.trelent.net/auth/logout';
 export const WRITE_DOCSTRING_URL        = 'https://prod-api.trelent.net/docs/docstring';
 
 // Dev testing
