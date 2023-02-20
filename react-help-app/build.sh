6f4d3db6a09705d2c635d997ed9033055d7e6b32
Coolroo <coolrooswag@gmail.com>
2023-02-20 17:09:14 +0000

    Fixed dependency scipt

--- react-help-app/build.sh
+++ react-help-app/build.sh
@@ -1,3 +1,3 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "${SCRIPT_DIR}/react-help-app"
+#! /bin/bash
+cd "react-help-app"
 node ./scripts/build-non-split.js
